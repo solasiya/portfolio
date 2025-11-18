cd16ed9f6672f87d3a883ae97759be7788b66611
kumkaniasiya <solomonasiya@gmail.com>
2025-11-18 13:03:03 +0200

    Fix JSXDEV error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import **React**, { StrictMode } from "react"; // Add React here
+import React, { StrictMode } from "react"; // Explicit React import
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "@fontsource/geist-sans/400.css"; // Specific weight
-import "@fontsource/geist-sans/700.css"; // Bold
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
